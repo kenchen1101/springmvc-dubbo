9137051d7d9486c0a56d7ae4a924d771220deb6a
Administrator <Administrator@10.0.0.33>
2016-03-06 20:13:01 +0800

    commit

--- rpc-server/ddl/oracle/auth.sql
+++ rpc-server/ddl/oracle/auth.sql
@@ -1,6 +1,6 @@
 create table t_auth_permission 
 (
-   id                   varchar2(32)                   not null,
+   id                   varchar2(32)                   primary key not null,
    menu_name            varchar2(50)                   null,
    menu_code            varchar2(50)                   not null,
    url                  varchar2(32)                   null,
@@ -11,8 +11,7 @@ create table t_auth_permission
    remark               varchar2(200)                  null,
    create_time          timestamp                      not null,
    update_time          timestamp                      null,
-   is_del               number(10)                     not null default '0',
-   constraint pk_t_auth_permission primary key clustered (id)
+   is_del               number(10)                     default '0' not null
 );
 comment on table t_auth_permission is '菜单许可表';
 comment on column t_auth_permission.id is '主键：菜单许可ID';
@@ -27,11 +26,10 @@ comment on column t_auth_permission.remark is '备注';
 comment on column t_auth_permission.create_time is '创建时间';
 comment on column t_auth_permission.update_time is '编辑时间';
 comment on column t_auth_permission.is_del is '是否删除:1=已删除;0=未删除';
-comment on primary key on t_auth_permission is '菜单许可表主键';
 
 create table t_auth_role 
 (
-   id                   varchar2(32)                   not null,
+   id                   varchar2(32)                   primary key not null,
    role_name            varchar2(50)                   null,
    role_code            varchar2(50)                   not null,
    business_system      varchar2(20)                   null,
@@ -39,8 +37,7 @@ create table t_auth_role
    remark               varchar2(200)                  null,
    create_time          timestamp                      not null,
    update_time          timestamp                      null,
-   is_del               number(10)                     not null default '0',
-   constraint pk_t_auth_role primary key clustered (id)
+   is_del               number(10)                     default '0' not null 
 );
 comment on table t_auth_role is '角色表';
 comment on column t_auth_role.id is '主键：角色ID';
@@ -52,18 +49,16 @@ comment on column t_auth_role.remark is '备注';
 comment on column t_auth_role.create_time is '创建时间';
 comment on column t_auth_role.update_time is '编辑时间';
 comment on column t_auth_role.is_del is '是否删除:1=已删除;0=未删除';
-comment on primary key on t_auth_role is '后台角色表主键';
 
 create table t_auth_role_permission 
 (
-   id                   varchar2(32)                   not null,
+   id                   varchar2(32)                   primary key not null,
    role_id              varchar2(32)                   not null,
    permission_id        varchar2(32)                   null,
    business_system      varchar2(20)                   null,
    create_time          timestamp                      not null,
    update_time          timestamp                      null,
-   is_del               number(10)                     not null default '0',
-   constraint pk_t_auth_role_permission primary key clustered (id)
+   is_del               number(10)                     default '0' not null 
 );
 comment on table t_auth_role_permission is '角色菜单许可关系表';
 comment on column t_auth_role_permission.id is '主键：用户角色ID';
@@ -73,11 +68,10 @@ comment on column t_auth_role_permission.business_system is '业务系统';
 comment on column t_auth_role_permission.create_time is '创建时间';
 comment on column t_auth_role_permission.update_time is '编辑时间';
 comment on column t_auth_role_permission.is_del is '是否删除:1=已删除;0=未删除';
-comment on primary key on t_auth_role_permission is '用户角色关系表主键';
 
 create table t_auth_user 
 (
-   id                   varchar2(32)                   not null,
+   id                   varchar2(32)                   primary key not null,
    email                varchar2(50)                   null,
    login_name           varchar2(50)                   not null,
    user_name            varchar2(50)                   not null,
@@ -85,8 +79,7 @@ create table t_auth_user
    salt                 varchar2(50)                   null,
    create_time          timestamp                      not null,
    update_time          timestamp                      null,
-   is_del               number(10)                     not null default '0',
-   constraint pk_t_auth_user primary key clustered (id)
+   is_del               number(10)                     default '0' not null 
 );
 comment on table t_auth_user is '后台用户表';
 comment on column t_auth_user.id is '主键：用户ID';
@@ -98,17 +91,15 @@ comment on column t_auth_user.salt is 'salt码';
 comment on column t_auth_user.create_time is '创建时间';
 comment on column t_auth_user.update_time is '编辑时间';
 comment on column t_auth_user.is_del is '是否删除:1=已删除;0=未删除';
-comment on primary key on t_auth_user is '后台用户表主键';
 
 create table t_auth_user_role 
 (
-   id                   varchar2(32)                   not null,
+   id                   varchar2(32)                   primary key not null,
    role_id              varchar2(32)                   null,
    user_id              varchar2(32)                   not null,
    create_time          timestamp                      not null,
    update_time          timestamp                      null,
-   is_del               number(10)                     not null default '0',
-   constraint pk_t_auth_user_role primary key clustered (id)
+   is_del               number(10)                     default '0' not null 
 );
 comment on table t_auth_user_role is '用户角色关系表';
 comment on column t_auth_user_role.id is '主键：用户角色ID';
@@ -117,4 +108,3 @@ comment on column t_auth_user_role.user_id is '外键：用户ID';
 comment on column t_auth_user_role.create_time is '创建时间';
 comment on column t_auth_user_role.update_time is '编辑时间';
 comment on column t_auth_user_role.is_del is '是否删除:1=已删除;0=未删除';
-comment on primary key on t_auth_user_role is '用户角色关系表主键';
